dfd47077a8d1aa7cd88692aa02934d6d744c9364
huiruo <2196411859@qq.com>
2020-12-10 23:45:47 +0800

    refactor:目录

--- Booking_back/src/main/java/com/booking/controller/UserController.java
+++ Booking_back/src/main/java/com/booking/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.booking.controller;
 
-import com.booking.entity.JsonResult;
-import com.booking.service.UserService;
 import com.booking.entity.Result;
+import com.booking.service.UserService;
 import com.booking.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,40 +20,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    //***登录
     @RequestMapping("login")
-    public JsonResult<User> login(@RequestBody User user, HttpServletRequest request) {
+    public Result<User> login(@RequestBody User user, HttpServletRequest request) {
         User entityUser = new User();
-        log.info("登录user" + user);
         try {
             User userDb = userService.login(user);
             entityUser.setAccount(userDb.getAccount()).setEmail(userDb.getEmail()).setId(userDb.getId());
-            log.info("返回user" + entityUser);
             //登录成功保存标记:方式1：存在 ServletContext application(暂用服务器资源) 2.Redis: 以userid为标记
             request.getServletContext().setAttribute(userDb.getId(), userDb);
+            return new Result<>(entityUser);
         } catch (Exception e) {
-            //result.setCode(1).setMsg(e.getMessage());
-            return new JsonResult<>(1,e.getMessage());
+            return new Result<>(1, e.getMessage());
         }
-        return new JsonResult<>(entityUser);
     }
 
     @RequestMapping("register")
-    public Result register(@RequestBody User user, HttpServletRequest request) {
-        Result result = new Result();
-        log.info("register1" + user);
+    public Result<User> register(@RequestBody User user, HttpServletRequest request) {
         try {
-            //result.setCode(1).setMsg("注册成功").setUserId("183");
-            /*User  userDb = userService.login(user);
-            result.setMsg("登录成功").setUserId(userDb.getId());
-            //登录成功保存标记:  方式1：存在 ServletContext application(暂用服务器资源) 2.Redis: 以userid为标记
-            request.getServletContext().setAttribute(userDb.getId(),userDb);
-             */
-            //**注册用户
             userService.register(user);
+            return new Result<>();
         } catch (Exception e) {
-            result.setCode(0).setMsg(e.getMessage());
+            return new Result<>(1, e.getMessage());
         }
-        return result;
     }
 }
--- Booking_back/src/main/java/com/booking/entity/Result.java
+++ Booking_back/src/main/java/com/booking/entity/Result.java
@@ -1,17 +1,54 @@
 package com.booking.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import lombok.experimental.Accessors;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@Accessors(chain=true)
-public class Result {
+public class Result<T> {
+    private T data;
     private int code;
     private String msg;
+    private static final int constCode = 0;
+
+    /**
+     * 若没有数据返回，默认状态码为 0，提示信息为“操作成功！”
+     */
+    public Result() {
+        this.code = constCode;
+        this.msg = "操作成功！";
+    }
+
+    /**
+     * 若没有数据返回，可以人为指定状态码和提示信:错误
+     *
+     * @param code
+     * @param msg
+     */
+    public Result(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    /**
+     * 有数据返回时，状态码为 0，默认提示信息为“操作成功！”
+     *
+     * @param data
+     */
+    public Result(T data) {
+        this.data = data;
+        this.code = constCode;
+        this.msg = "操作成功！";
+    }
+
+    /**
+     * 有数据返回，状态码为 0
+     *
+     * @param data
+     * @param msg
+     */
+    public Result(T data, String msg) {
+        this.data = data;
+        this.code = constCode;
+        this.msg = msg;
+    }
+    // 省略 get 和 set 方法:lombok代替
 }
